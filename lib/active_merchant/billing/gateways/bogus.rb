ef38d1a211e1735d832360335172c86701a76e75
Siyuan He <siyuan.he@maintenanceassistant.com>
2012-04-18 14:43:46 -0400

    update on Bogus.credit_card_detail

--- lib/active_merchant/billing/gateways/bogus.rb
+++ lib/active_merchant/billing/gateways/bogus.rb
@@ -3,7 +3,7 @@ module ActiveMerchant #:nodoc:
     # Bogus Gateway
     class BogusGateway < Gateway
       AUTHORIZATION = '1'
-      
+
       SUCCESS_MESSAGE = "Bogus Gateway: Forced success"
       FAILURE_MESSAGE = "Bogus Gateway: Forced failure"
       ERROR_MESSAGE = "Bogus Gateway: Use CreditCard number 1 for success, 2 for exception and anything else for error"
@@ -12,13 +12,13 @@ module ActiveMerchant #:nodoc:
       CAPTURE_ERROR_MESSAGE = "Bogus Gateway: Use authorization number 1 for exception, 2 for error and anything else for success"
       VOID_ERROR_MESSAGE = "Bogus Gateway: Use authorization number 1 for exception, 2 for error and anything else for success"
       REFUND_ERROR_MESSAGE = "Bogus Gateway: Use trans_id number 1 for exception, 2 for error and anything else for success"
-      
+
       self.supported_countries = ['US']
       self.supported_cardtypes = [:bogus]
       self.homepage_url = 'http://example.com'
       self.display_name = 'Bogus'
-      
-      
+
+
       def authorize(money, credit_card_or_reference, options = {})
         money = amount(money)
         case normalize(credit_card_or_reference)
@@ -28,9 +28,9 @@ module ActiveMerchant #:nodoc:
           Response.new(false, FAILURE_MESSAGE, {:authorized_amount => money, :error => FAILURE_MESSAGE }, :test => true)
         else
           raise Error, ERROR_MESSAGE
-        end      
+        end
       end
-  
+
       def purchase(money, credit_card_or_reference, options = {})
         money = amount(money)
         case normalize(credit_card_or_reference)
@@ -42,7 +42,7 @@ module ActiveMerchant #:nodoc:
           raise Error, ERROR_MESSAGE
         end
       end
- 
+
       def create_profile(options = {})
 
         case normalize( options[:credit_card])
@@ -54,12 +54,12 @@ module ActiveMerchant #:nodoc:
           raise Error, ERROR_MESSAGE
         end
       end
- 
+
       def update_profile(profile_id, options = {})
         case profile_id
         when "pid1"
           Response.new(true, SUCCESS_MESSAGE, {:ProfileID => "pid1"}, :test => true)
-        else 
+        else
           raise Error, ERROR_MESSAGE
         end
       end
@@ -68,7 +68,7 @@ module ActiveMerchant #:nodoc:
         case profile_id
         when "pid1"
           Response.new(true, SUCCESS_MESSAGE, {:ProfileID => "pid1"}, :test => true)
-        else 
+        else
           raise Error, ERROR_MESSAGE
         end
       end
@@ -77,7 +77,7 @@ module ActiveMerchant #:nodoc:
         case profile_id
         when "pid1"
           Response.new(true, SUCCESS_MESSAGE, {:ProfileID => "pid1"}, :test => true)
-        else 
+        else
           raise Error, ERROR_MESSAGE
         end
       end
@@ -111,12 +111,12 @@ module ActiveMerchant #:nodoc:
           Response.new(true, SUCCESS_MESSAGE, {:paid_amount => money}, :test => true)
         end
       end
- 
+
       def capture(money, reference, options = {})
         money = amount(money)
         case reference
         when '1'
-          Response.new(true, SUCCESS_MESSAGE, {:paid_amount => money}, :test => true)          
+          Response.new(true, SUCCESS_MESSAGE, {:paid_amount => money}, :test => true)
         when '2'
           Response.new(false, FAILURE_MESSAGE, {:paid_amount => money, :error => FAILURE_MESSAGE }, :test => true)
         else
@@ -134,7 +134,7 @@ module ActiveMerchant #:nodoc:
           Response.new(true, SUCCESS_MESSAGE, {:authorization => reference}, :test => true)
         end
       end
-      
+
       def store(credit_card_or_reference, options = {})
         case normalize(credit_card_or_reference)
         when '1'
@@ -143,9 +143,9 @@ module ActiveMerchant #:nodoc:
           Response.new(false, FAILURE_MESSAGE, {:billingid => nil, :error => FAILURE_MESSAGE }, :test => true)
         else
           raise Error, ERROR_MESSAGE
-        end              
+        end
       end
-      
+
       def unstore(reference, options = {})
         case reference
         when '1'
@@ -158,25 +158,42 @@ module ActiveMerchant #:nodoc:
       end
 
       def credit_card_detail(reference)
-        Response.new(true, SUCCESS_MESSAGE, 
-          {:credit_card => {
-            "type"       => "visa",
-            "first_name" => "test_first_name",
-            "last_name"  => "test_last_name",
-            "number"     => "************1234",
-            "month"       => "02",
-            "year"      => "2017"
-          }}, :test =>true )
-      end
-      private
 
-      def normalize(credit_card_or_reference)
-        if credit_card_or_reference.respond_to?(:number)
-          credit_card_or_reference.number
-        else
-          credit_card_or_reference.to_s
+
+        case reference
+        when '1'
+          Response.new(true, SUCCESS_MESSAGE,
+            {:credit_card => {
+              "type"       => "visa",
+              "first_name" => "test_first_name",
+              "last_name"  => "test_last_name",
+              "number"     => "************1234",
+              "month"       => "02",
+              "year"      => "2012"
+            }}, :test =>true )
+          else
+            Response.new(true, SUCCESS_MESSAGE,
+              {:credit_card => {
+                "type"       => "visa",
+                "first_name" => "test_first_name",
+                "last_name"  => "test_last_name",
+                "number"     => "************1234",
+                "month"       => "02",
+                "year"      => "2017"
+              }}, :test =>true )
+            
+          end
+        end
+
+        private
+
+        def normalize(credit_card_or_reference)
+          if credit_card_or_reference.respond_to?(:number)
+            credit_card_or_reference.number
+          else
+            credit_card_or_reference.to_s
+          end
         end
       end
     end
   end
-end
